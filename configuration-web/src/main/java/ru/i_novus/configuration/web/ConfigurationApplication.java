41cb88abd7e72dd74883932f0cbaffe3842ba7b9
iryabov <iryabov@i-novus.ru>
2019-08-15 11:51:11 +0300

    EPMP-16 web модуль

---
+++ configuration-web/src/main/java/ru/i_novus/configuration/web/ConfigurationApplication.java
@@ -0,0 +1,11 @@
+package ru.i_novus.configuration.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationApplication.class);
+    }
+}
