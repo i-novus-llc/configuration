a5aa651f5c05dda8a8c0afdb6c26747d7fd832dc
iryabov <iryabov@i-novus.ru>
2019-09-19 17:12:10 +0300

    EPMP-172 Возвращаю id вместе с code в children

--- config-web/src/main/java/ru/i_novus/config/web/ApplicationSystemUtil.java
+++ config-web/src/main/java/ru/i_novus/config/web/ApplicationSystemUtil.java
@@ -1,10 +1,17 @@
 package ru.i_novus.config.web;
 
+import net.n2oapp.criteria.dataset.DataSet;
+
 import java.util.List;
 
 public class ApplicationSystemUtil {
 
-    public static <T extends List> T get(T item) {
-        return (item == null || item.isEmpty()) ? null : item;
+    public static <T extends List> T normalize(T children) {
+        if (children == null || children.isEmpty())
+            return null;
+        for (Object item : children) {
+            ((DataSet)item).put("id", ((DataSet)item).get("code"));
+        }
+        return children;
     }
 }
