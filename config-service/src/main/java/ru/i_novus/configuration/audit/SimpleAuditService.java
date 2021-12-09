705019e3d6ec41ee61932062acc48e6266722cca
ilebedenko <ilebedenko@i-novus.ru>
2021-12-09 14:05:28 +0300

    [CF][CONFIG-27] - rename base audit service

--- config-service/src/main/java/ru/i_novus/configuration/audit/SimpleAuditService.java
+++ config-service/src/main/java/ru/i_novus/configuration/audit/SimpleAuditService.java
@@ -6,7 +6,7 @@ import ru.i_novus.config.api.util.AuditService;
 
 @Component
 @Slf4j
-public class BaseAuditServiceImpl implements AuditService {
+public class SimpleAuditService implements AuditService {
     @Override
     public void audit(String action, Object object, String objectId, String objectName) {
         log.info(String.format("Действие '%s' было произведено над объектом '%s' с идентификатором '%s'", action, objectName, objectId));
