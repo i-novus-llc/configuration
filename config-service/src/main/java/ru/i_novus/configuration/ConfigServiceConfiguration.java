f6edf81181479372cc2917f0ccacc5779d3cf221
estartcev <estartcev@i-novus.ru>
2022-10-03 10:01:17 +0300

    nf/CONFIG-46 добавлена поддержка yaml

--- config-service/src/main/java/ru/i_novus/configuration/ConfigServiceConfiguration.java
+++ config-service/src/main/java/ru/i_novus/configuration/ConfigServiceConfiguration.java
@@ -1,6 +1,7 @@
 package ru.i_novus.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.consul.config.ConsulConfigProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,9 +13,10 @@ import ru.i_novus.configuration.config.service.YamlConfigValueServiceConsulImpl;
 public class ConfigServiceConfiguration {
 
     @Bean
-    public ConfigValueService configValueService(@Value("${config.consul.yaml.enabled:false}") boolean yamlEnabled) {
+    public ConfigValueService configValueService(@Value("${spring.cloud.consul.config.format:KEY_VALUE}")
+                                                         ConsulConfigProperties.Format format) {
         final RestTemplate restTemplate = new RestTemplate();
-        return yamlEnabled ?
+        return format == ConsulConfigProperties.Format.YAML ?
                 new YamlConfigValueServiceConsulImpl(restTemplate) :
                 new ConfigValueServiceConsulImpl(restTemplate);
     }
